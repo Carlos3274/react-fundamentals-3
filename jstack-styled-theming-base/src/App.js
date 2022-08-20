d6085b0d057dd7855304ce622655d3bb619879d1
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-08-20 18:16:57 -0300

    block11 finalizado

--- jstack-styled-theming-base/src/App.js
+++ jstack-styled-theming-base/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
-import GlobalStyle from './styles/global';
-import Layout from './components/Layout';
+import GlobalStyle from './styles/global'
+import Layout from './components/Layout'
+
+import themes from './styles/themes'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={themes.dark}>
       <GlobalStyle />
       <Layout />
-    </>
-  );
-};
+    </ThemeProvider>
+  )
+}
 
-export default App;
+export default App
